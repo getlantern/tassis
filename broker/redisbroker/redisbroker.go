9bf1274c91dd1680fad6b690fa58e4822ed4c0f6
Ox Cart <ox.to.a.cart@gmail.com>
2021-02-16 07:56:15 -0600

    Added function for capping size of redis message streams

--- broker/redisbroker/redisbroker.go
+++ broker/redisbroker/redisbroker.go
@@ -1,3 +1,20 @@
+// redisbroker implements the ../broker.Broker interface using Redis streams.
+//
+// Each Topic gets its own stream at topic:{<topic>}, for example if the topic is "abcde" the stream is at key "topic:{abcde}".
+// The curly braces around the topic indicate that the topic is used as the sharding key when running on a Redis cluster.
+//
+// Streams are append-only logs from which clients may read starting at any offset, where the offset is determined by the ID
+// of the message stored in the stream. redisbroker takes care of tracking the highest previously acknowledged offset by
+// stream. This is stored in offset:<stream>, for example "offset:topic:{abcde}".
+//
+// Whenever a new subscriber is created, it will start receiving messages at the highest recorded offset. Whenever a message is
+// acked, the highest recorded offset is updated to the acked ID. However, if an ACK is received out of order, it is ignored.
+//
+// The broker also supports a stream capping function that can be used in a single batch process. This capping function caps
+// streams to a certain length to limit storage, deleting the oldest messages if necessary to stay under the cap.
+//
+// In practice, this scheme provides at least once delivery semantics as long as messages haven't been lost due to hitting the
+// cap before they could be delivered.
 package redisbroker
 
 import (
@@ -16,82 +33,6 @@ import (
 var (
 	log = golog.LoggerFor("redisdb")
 )
-
-type message struct {
-	b    *redisBroker
-	sub  *subscriber
-	id   string
-	data []byte
-}
-
-func (msg *message) Data() []byte {
-	return msg.data
-}
-
-func (msg *message) Acker() func() error {
-	a := &acker{
-		b:   msg.b,
-		sub: msg.sub,
-		id:  msg.id,
-	}
-	return a.ack
-}
-
-type acker struct {
-	b   *redisBroker
-	sub *subscriber
-	id  string
-}
-
-func (a *acker) ack() error {
-	a.sub.highOffsetMx.Lock()
-	shouldAck := a.id >= a.sub.highOffset
-	a.sub.highOffsetMx.Unlock()
-	if shouldAck {
-		return a.b.client.Set(context.Background(), offsetName(a.sub.stream), a.id, 0).Err()
-	}
-	return nil
-}
-
-type subscriber struct {
-	id           int64
-	b            *redisBroker
-	stream       string
-	messages     chan broker.Message
-	highOffset   string
-	highOffsetMx sync.Mutex
-	closeOnce    sync.Once
-}
-
-func (sub *subscriber) Messages() <-chan broker.Message {
-	return sub.messages
-}
-
-func (sub *subscriber) Close() error {
-	sub.closeOnce.Do(func() {
-		sub.b.subscriberRemoved <- sub
-	})
-	return nil
-}
-
-type publisher struct {
-	b      *redisBroker
-	stream string
-}
-
-func (pub *publisher) Publish(data []byte) error {
-	ctx := context.Background()
-	return pub.b.client.XAdd(ctx, &redis.XAddArgs{
-		Stream: pub.stream,
-		Values: map[string]interface{}{
-			"data": string(data),
-		},
-	}).Err()
-}
-
-func (pub *publisher) Close() error {
-	return nil
-}
 
 type redisBroker struct {
 	client            *redis.Client
@@ -226,10 +167,86 @@ func (b *redisBroker) NewPublisher(topicName string) (broker.Publisher, error) {
 	}, nil
 }
 
+type message struct {
+	b    *redisBroker
+	sub  *subscriber
+	id   string
+	data []byte
+}
+
+func (msg *message) Data() []byte {
+	return msg.data
+}
+
+func (msg *message) Acker() func() error {
+	a := &acker{
+		b:   msg.b,
+		sub: msg.sub,
+		id:  msg.id,
+	}
+	return a.ack
+}
+
+type acker struct {
+	b   *redisBroker
+	sub *subscriber
+	id  string
+}
+
+func (a *acker) ack() error {
+	a.sub.highOffsetMx.Lock()
+	shouldAck := a.id >= a.sub.highOffset
+	a.sub.highOffsetMx.Unlock()
+	if shouldAck {
+		return a.b.client.Set(context.Background(), offsetName(a.sub.stream), a.id, 0).Err()
+	}
+	return nil
+}
+
+type subscriber struct {
+	id           int64
+	b            *redisBroker
+	stream       string
+	messages     chan broker.Message
+	highOffset   string
+	highOffsetMx sync.Mutex
+	closeOnce    sync.Once
+}
+
+func (sub *subscriber) Messages() <-chan broker.Message {
+	return sub.messages
+}
+
+func (sub *subscriber) Close() error {
+	sub.closeOnce.Do(func() {
+		sub.b.subscriberRemoved <- sub
+	})
+	return nil
+}
+
+type publisher struct {
+	b      *redisBroker
+	stream string
+}
+
+func (pub *publisher) Publish(data []byte) error {
+	ctx := context.Background()
+	return pub.b.client.XAdd(ctx, &redis.XAddArgs{
+		Stream: pub.stream,
+		Values: map[string]interface{}{
+			"data": string(data),
+		},
+	}).Err()
+}
+
+func (pub *publisher) Close() error {
+	return nil
+}
+
 func streamName(topicName string) string {
 	return "topic:{" + topicName + "}"
 }
 
 func offsetName(stream string) string {
-	return stream + ":offset"
+	return "offset:" + stream
 }
