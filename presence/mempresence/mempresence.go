513c7a0055033c61206127d3bae0456163b9894c
Ox Cart <ox.to.a.cart@gmail.com>
2021-02-19 13:46:08 -0600

    Protecting mempresence with mutex

--- presence/mempresence/mempresence.go
+++ presence/mempresence/mempresence.go
@@ -2,23 +2,34 @@
 package mempresence
 
 import (
+	"sync"
+
 	"github.com/getlantern/tassis/model"
 	"github.com/getlantern/tassis/presence"
 )
 
-type repo map[string]string
+type repo struct {
+	m  map[string]string
+	mx sync.Mutex
+}
 
 func NewRepository() presence.Repository {
-	return repo(make(map[string]string, 0))
+	return repo{m: make(map[string]string, 0)}
 }
 
 func (r repo) Announce(addr *model.Address, tassisHost string) error {
-	r[addr.String()] = tassisHost
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	r.m[addr.String()] = tassisHost
 	return nil
 }
 
 func (r repo) Find(addr *model.Address) (string, error) {
-	tassisHost, found := r[addr.String()]
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	tassisHost, found := r.m[addr.String()]
 	if !found {
 		return "", model.ErrUnknownDevice
 	}
