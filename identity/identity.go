3d75e3178b694b5f0a7a954ecb6afe115ea7085f
Ox Cart <ox.to.a.cart@gmail.com>
2021-03-13 09:36:11 -0600

    Using Curve25519 instead of ed25519 for IdentityKey

--- identity/identity.go
+++ identity/identity.go
@@ -6,15 +6,16 @@ import (
 	"crypto/rand"
 
 	"github.com/getlantern/tassis/encoding"
+	"github.com/jorrizza/ed2curve25519"
 )
 
-// PublicKey is an ed25519.PublicKey that is 32 bytes long
-type PublicKey ed25519.PublicKey
+// PublicKey is a 32 byte Curve25519 (x25519) public key
+type PublicKey []byte
 
-// PrivateKey is an ed25519.PrivateKey
-type PrivateKey ed25519.PrivateKey
+// PrivateKey is an Ed25519 private key
+type PrivateKey []byte
 
-// KeyPair is an ed25519 key pair
+// KeyPair is a key pair with a Curve25519 public key and the corresponding Ed25519 private key
 type KeyPair struct {
 	Public  PublicKey
 	Private PrivateKey
@@ -26,8 +27,9 @@ func GenerateKeyPair() (*KeyPair, error) {
 	if err != nil {
 		return nil, err
 	}
+	x25519key := ed2curve25519.Ed25519PublicKeyToCurve25519(public)
 	return &KeyPair{
-		Public:  PublicKey(public),
+		Public:  PublicKey(x25519key),
 		Private: PrivateKey(private),
 	}, nil
 }
@@ -37,9 +39,44 @@ func (priv PrivateKey) Sign(data []byte) ([]byte, error) {
 	return ed25519.PrivateKey(priv).Sign(rand.Reader, data, crypto.Hash(0))
 }
 
-// Verifies the given signature on the given data using this PublicKey
+// Verifies the given signature on the given data using the Ed25519 version of this Curve25519
+// Public Key
 func (pub PublicKey) Verify(data, signature []byte) bool {
-	return ed25519.Verify(ed25519.PublicKey(pub), data, signature)
+	var key [32]byte
+	copy(key[:], pub)
+
+	// below code from https://stackoverflow.com/questions/62586488/how-do-i-sign-a-curve25519-key-in-golang
+	key[31] &= 0x7F
+
+	/* Convert the Curve25519 public key into an Ed25519 public key.  In
+	particular, convert Curve25519's "montgomery" x-coordinate into an
+	Ed25519 "edwards" y-coordinate:
+	ed_y = (mont_x - 1) / (mont_x + 1)
+	NOTE: mont_x=-1 is converted to ed_y=0 since fe_invert is mod-exp
+	Then move the sign bit into the pubkey from the signature.
+	*/
+
+	var edY, one, montX, montXMinusOne, montXPlusOne FieldElement
+	FeFromBytes(&montX, &key)
+	FeOne(&one)
+	FeSub(&montXMinusOne, &montX, &one)
+	FeAdd(&montXPlusOne, &montX, &one)
+	FeInvert(&montXPlusOne, &montXPlusOne)
+	FeMul(&edY, &montXMinusOne, &montXPlusOne)
+
+	var A_ed [32]byte
+	FeToBytes(&A_ed, &edY)
+
+	A_ed[31] |= signature[63] & 0x80
+	signature[63] &= 0x7F
+
+	var sig = make([]byte, 64)
+	var aed = make([]byte, 32)
+
+	copy(sig, signature[:])
+	copy(aed, A_ed[:])
+
+	return ed25519.Verify(aed, data, signature)
 }
 
 func (pub PublicKey) String() string {
