17c5bc9d7b1041f2f530f6acf61305a4b8d8726a
Ox Cart <ox.to.a.cart@gmail.com>
2022-04-20 10:19:48 -0500

    Added reporting of host and runtime metrics to Honeycomb

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -3,9 +3,10 @@ package telemetry
 import (
 	"context"
 	"os"
-	"time"
 
 	"github.com/lightstep/otel-launcher-go/launcher"
+	hostMetrics "go.opentelemetry.io/contrib/instrumentation/host"
+	runtimeMetrics "go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
@@ -37,7 +38,6 @@ func Start() func() {
 		log.Debug("Will report traces and metrics to Lighstep")
 		ls := launcher.ConfigureOpentelemetry(
 			launcher.WithServiceName("tassis"),
-			launcher.WithMetricReportingPeriod(100*time.Millisecond),
 			launcher.WithAccessToken(lighstepKey),
 		)
 		return func() { ls.Shutdown() }
@@ -102,7 +102,8 @@ func initHoneycombMetrics(honeycombKey string) func() {
 	opts := []otlpmetricgrpc.Option{
 		otlpmetricgrpc.WithEndpoint("api.honeycomb.io:443"),
 		otlpmetricgrpc.WithHeaders(map[string]string{
-			"x-honeycomb-team": honeycombKey,
+			"x-honeycomb-team":    honeycombKey,
+			"x-honeycomb-dataset": "tassis",
 		}),
 		otlpmetricgrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")),
 	}
@@ -130,6 +131,19 @@ func initHoneycombMetrics(honeycombKey string) func() {
 		controller.WithExporter(exporter),
 		controller.WithResource(resource),
 	)
+	if startErr := c.Start(context.Background()); startErr != nil {
+		log.Errorf("Unable to start metrics controller, will not report metrics to Honeycomb: %v", startErr)
+		return func() {}
+	}
+	if startErr := runtimeMetrics.Start(runtimeMetrics.WithMeterProvider(c)); startErr != nil {
+		log.Errorf("failed to start runtime metrics: %v", startErr)
+		return func() {}
+	}
+
+	if startErr := hostMetrics.Start(hostMetrics.WithMeterProvider(c)); startErr != nil {
+		log.Errorf("failed to start host metrics: %v", startErr)
+		return func() {}
+	}
 
 	global.SetMeterProvider(c)
 	return func() {
