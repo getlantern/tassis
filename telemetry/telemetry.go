37193cd73544cea3b523830bc7ca59c917059871
orca <orca@getlantern.org>
2022-07-12 10:46:09 -0700

    test telemetry as http to lantr.net (optionally: iantem.io)

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -2,14 +2,16 @@ package telemetry
 
 import (
 	"context"
+	"crypto/tls"
 
+	"github.com/getlantern/keyman"
 	hostMetrics "go.opentelemetry.io/contrib/instrumentation/host"
 	runtimeMetrics "go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/global"
 	"go.opentelemetry.io/otel/metric/instrument"
@@ -31,10 +33,24 @@ var (
 	MessagesSent syncfloat64.Counter
 )
 
+var caCert = `
+-----BEGIN CERTIFICATE-----
+MIIB2TCCAX+gAwIBAgIUALZZcdj7gI5fPFtaMc1JzBSGtEkwCgYIKoZIzj0EAwIw
+KDESMBAGA1UEChMJTGFudGVybmV0MRIwEAYDVQQDEwlsYW50ZXJuZXQwHhcNMjIw
+NTA1MDgzMDIwWhcNMzIwNTAyMDgzMDE5WjAoMRIwEAYDVQQKEwlMYW50ZXJuZXQx
+EjAQBgNVBAMTCWxhbnRlcm5ldDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABPQ7
+JmXBOWjoXZsKHoAAA4fdnZHPE2aIftlVWgVOqsUqXJHfcm0RjgDJ2E0oME4DMsFw
+HOiqRZJYYVrN3jGnQ/ujgYYwgYMwDgYDVR0PAQH/BAQDAgEGMB0GA1UdJQQWMBQG
+CCsGAQUFBwMBBggrBgEFBQcDAjASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1UdDgQW
+BBT//YqPe5Su0DMo/SOWQLcyOFI0VzAfBgNVHSMEGDAWgBT//YqPe5Su0DMo/SOW
+QLcyOFI0VzAKBggqhkjOPQQDAgNIADBFAiEAlwyNsSgcy6/jvBXKSrfYMm/ef1eP
+Nr0cfu9U5QFcClkCICEk02jI+ANnoGkbh1TEFK60JJadHDu7pLwe9wmUdF0P
+-----END CERTIFICATE-----
+`
+
 // Start configures opentelemetry for collecting metrics and traces, and returns
 // a function to shut down telemetry collection.
 // StartTeleport() does this for Teleport, an OTEL gateway collector.
-// Start() does this for direct export to Lightstep and Honeycomb (if key(s) provided).
 
 func StartTeleport() func() {
 	// Teleport ingests telemetry data for subsequent processing and export to downstream services.
@@ -52,12 +68,19 @@ func StartTeleport() func() {
 
 func initTeleportTracing() func() {
 	log.Debug("Will report traces to Teleport")
-	// Create gRPC client to talk to Teleport, our OTEL gateway collector
-	opts := []otlptracegrpc.Option{
-		otlptracegrpc.WithInsecure(),
-		otlptracegrpc.WithEndpoint("127.0.0.1:4317"),
+	// Create http client to talk to Teleport, our OTEL gateway collector
+
+	cert, err := keyman.LoadCertificateFromPEMBytes([]byte(caCert))
+	if err != nil {
+		panic(err)
 	}
-	client := otlptracegrpc.NewClient(opts...)
+	opts := []otlptracehttp.Option{
+		otlptracehttp.WithEndpoint("telemetry.lantr.net"),
+		otlptracehttp.WithTLSClientConfig(&tls.Config{
+			RootCAs: cert.PoolContainingCert(),
+		}),
+	}
+	client := otlptracehttp.NewClient(opts...)
 
 	// Create an exporter that exports to the Honeycomb OTEL collector
 	exporter, err := otlptrace.New(context.Background(), client)
@@ -89,12 +112,19 @@ func initTeleportTracing() func() {
 func initTeleportMetrics() func() {
 	log.Debug("Will report metrics to Teleport")
 
-	// Create gRPC client to talk to Teleport, our OTEL gateway collector
-	opts := []otlpmetricgrpc.Option{
-		otlpmetricgrpc.WithInsecure(),
-		otlpmetricgrpc.WithEndpoint("127.0.0.1:4317"),
+	cert, err := keyman.LoadCertificateFromPEMBytes([]byte(caCert))
+	if err != nil {
+		panic(err)
 	}
-	client := otlpmetricgrpc.NewClient(opts...)
+
+	// Create http client to talk to Teleport, our OTEL gateway collector
+	opts := []otlpmetrichttp.Option{
+		otlpmetrichttp.WithEndpoint("telemetry.lantr.net"),
+		otlpmetrichttp.WithTLSClientConfig(&tls.Config{
+			RootCAs: cert.PoolContainingCert(),
+		}),
+	}
+	client := otlpmetrichttp.NewClient(opts...)
 
 	// Create an exporter that exports to the Honeycomb OTEL collector
 	exporter, err := otlpmetric.New(context.Background(), client)
